b1b83f215215aadbc9be07050bf6d5af5edd74d4
Samuel Martins <sbm.martins@gmail.com>
2021-04-14 10:33:22 -0300

    adding a enumerator for layer keys; coding the panel for mapping

--- vaumc/va.py
+++ vaumc/va.py
@@ -1,4 +1,5 @@
 import argparse
+from enum import Enum
 from typing import List
 
 import matplotlib.pyplot as plt
@@ -13,9 +14,19 @@ from keras.models import load_model
 from unsup_neuralnet.generic_autoencoder.reconstruct_image import reconstruct_image
 
 
+class MappingDirection(Enum):
+    INPUT_2_RECONSTRUCTION = 'input --> reconstruction'
+    RECONSTRUCTION_2_INPUT = 'reconstruction --> input'
+
+class LayerKey(Enum):
+    INPUT_IMAGE = 'input image'
+    MARKERS = 'markers'
+    RECONSTRUCTION = 'reconstruction'
+
+
+
 
 model = None
-
 
 
 def build_argparse():
@@ -62,14 +73,14 @@ def build_colors_from_colormap(cmap_name='Set3'):
 def image_filepicker(filename=Path(), clear_markers=True) -> List[napari.types.LayerDataTuple]:
     print("****************************")
     img = io.imread(filename)
-    out_layers = [(img, {'name': 'input image'})]
+    out_layers = [(img, {'name': LayerKey.INPUT_IMAGE.value})]
 
     rec_img_layer = reconstruct(img)
     out_layers.append(rec_img_layer)
 
     if clear_markers:
         blank = np.zeros(img.shape, dtype=np.int)
-        out_layers.append((blank, {'name': 'markers'}))
+        out_layers.append((blank, {'name': LayerKey.MARKERS.value}))
 
     return out_layers
 
@@ -81,7 +92,26 @@ def reconstruct(img: ImageData) -> napari.types.LayerDataTuple:
 
     rec_img = reconstruct_image(img, model)
 
-    return (rec_img, {'name': 'reconstruction'}, 'image')
+    return (rec_img, {'name': LayerKey.RECONSTRUCTION.value}, 'image')
+
+
+# If we use only the Enum as the type (without changing), a dropdown menu is also created but
+# the options are the enum names instead of their values: INPUT_2_RECONSTRUCTION and INPUT_2_RECONSTRUCTION
+# Although it works that way, I preferred to use their option values.
+@magicgui(call_button='Mapping',
+          direction={'choices': [MappingDirection.INPUT_2_RECONSTRUCTION.value,
+                                MappingDirection.RECONSTRUCTION_2_INPUT.value]})
+def mapping(viewer: napari.Viewer, direction=MappingDirection.INPUT_2_RECONSTRUCTION.value):
+    img = viewer.layers[LayerKey.INPUT_IMAGE.value]
+    markers = viewer.layers[LayerKey.MARKERS.value]
+    rec_img = viewer.layers[LayerKey.RECONSTRUCTION.value]
+
+    if direction == MappingDirection.INPUT_2_RECONSTRUCTION.value:
+        print('Forward Mapping')
+    else:
+        print('Reverse Mapping')
+
+
 
 
 if __name__ == '__main__':
@@ -99,10 +129,11 @@ if __name__ == '__main__':
 
         viewer.window.add_dock_widget(image_filepicker, area='left')
         viewer.window.add_dock_widget(reconstruct, area='left')
+        viewer.window.add_dock_widget(mapping, area='left')
 
-        reconstruct(viewer.layers['input image'].data)
+        reconstruct(viewer.layers[LayerKey.INPUT_IMAGE.value].data)
 
         blank = np.zeros(input_image.shape, dtype=np.int)
         napari_colors = build_colors_from_colormap(cmap_name='Set1')
-        viewer.add_labels(blank, name='markers', color=napari_colors)
+        viewer.add_labels(blank, name=LayerKey.MARKERS.value, color=napari_colors)
 
