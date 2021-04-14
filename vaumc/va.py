e5675be9ee34a1f81f09d3775d5ce97037400cbf
Samuel Martins <sbm.martins@gmail.com>
2021-04-14 09:34:49 -0300

    autoreconstruction when loading a new input image

--- vaumc/va.py
+++ vaumc/va.py
@@ -45,13 +45,27 @@ def validate_args(args):
         raise Exception(f'Invalid AutoEncoder2D extension: {args.model}\nTry *.h5')
 
 
+def build_colors_from_colormap(cmap_name='Set3'):
+    # list of RGB tuples (scale 0..1)
+    colors = list(plt.get_cmap(cmap_name).colors)
+    napari_colors = {}
+
+    for i, RGB in enumerate(colors):
+        napari_colors[i + 1] = tuple(list(RGB) + [1.0])  # RGBA
+
+    return napari_colors
+
+
 
 @magicgui(call_button='Load Input Image', filename={"filter": "Images (*.jpg *.jpeg *.png)"},
           clear_markers={'label': 'Clear the markers?'})
 def image_filepicker(filename=Path(), clear_markers=True) -> List[napari.types.LayerDataTuple]:
     print("****************************")
     img = io.imread(filename)
-    out_layers = [(img, {'name': 'input-image'})]
+    out_layers = [(img, {'name': 'input image'})]
+
+    rec_img_layer = reconstruct(img)
+    out_layers.append(rec_img_layer)
 
     if clear_markers:
         blank = np.zeros(img.shape, dtype=np.int)
@@ -70,38 +84,25 @@ def reconstruct(img: ImageData) -> napari.types.LayerDataTuple:
     return (rec_img, {'name': 'reconstruction'}, 'image')
 
 
-
-def build_colors_from_colormap(cmap_name='Set3'):
-    # list of RGB tuples (scale 0..1)
-    colors = list(plt.get_cmap(cmap_name).colors)
-    napari_colors = {}
-
-    for i, RGB in enumerate(colors):
-        napari_colors[i + 1] = tuple(list(RGB) + [1.0])  # RGBA
-
-    return napari_colors
-
-
 if __name__ == '__main__':
     parser = build_argparse()
     args = parser.parse_args()
     print_args(args)
 
-    np.random.seed(10)
-
     with napari.gui_qt():
         viewer = napari.Viewer()
 
         input_image = io.imread(args.input_image)
-        viewer.add_image(input_image, name='input-image')
+        viewer.add_image(input_image, name='input image')
 
         model = load_model(args.model)
 
         viewer.window.add_dock_widget(image_filepicker, area='left')
         viewer.window.add_dock_widget(reconstruct, area='left')
 
-        reconstruct(viewer.layers['input-image'].data)
+        reconstruct(viewer.layers['input image'].data)
 
         blank = np.zeros(input_image.shape, dtype=np.int)
         napari_colors = build_colors_from_colormap(cmap_name='Set1')
         viewer.add_labels(blank, name='markers', color=napari_colors)
+
