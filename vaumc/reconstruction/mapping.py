a83f1837d8347e369ecdfc43e048acd2008a7a9d
Samuel Martins <sbm.martins@gmail.com>
2021-04-14 15:58:32 -0300

    fixing errors in reconstruction.py; coding a module for mapping

---
+++ vaumc/reconstruction/mapping.py
@@ -0,0 +1,63 @@
+import numpy as np
+from skimage import io
+
+from .reconstruction import reconstruct_image_set
+from .util import normalization_value
+
+
+def forward_mapping(input_img, rec_img, markers, n_pertubations, model, debug=True):
+    roi = markers != 0
+    print(roi.shape)
+
+    max_range = int(normalization_value(input_img))  # e.g., 255 in 8-bit image
+
+    # step = (2 * max_range) / n_pertubations
+    # # the step may generate an extra value, so we only get the first `n_pertubations` numbers (float)
+    # pertubations = np.arange(-max_range, max_range + 1, step)[:n_pertubations]
+
+    pertubations = np.arange(-n_pertubations, n_pertubations + 1, 2)[:n_pertubations]
+    shape = tuple([n_pertubations] + list(input_img.shape))  # (n_pertubations, ysize, xsize)
+
+    # creates a numpy array with `n_pertubation` repetitions of the input_image
+    # X.shape ==> (n_pertubation, input_image.shape)
+    Xpert = np.zeros(shape, dtype='int')
+
+    for i, pert in enumerate(pertubations):
+        noise_img = np.array(input_img).astype('float')
+        noise_img[roi] += pert
+        noise_img[noise_img < 0] = 0
+        noise_img[noise_img > max_range] = max_range
+
+        Xpert[i] = noise_img.astype('int')
+
+    Xpert_rec = reconstruct_image_set(Xpert, model)
+
+
+    reps = tuple([n_pertubations] + [1] * input_img.ndim)
+    Xinput = np.tile(input_img, reps=reps)
+    Xrec = np.tile(rec_img, reps=reps)
+
+    numerator = np.abs(Xpert_rec - Xrec)
+    denominator = np.abs(Xpert - Xinput)
+    denominator[denominator == 0] = 1  # to avoid zero-division
+    influences = numerator / denominator
+    influences = influences.astype('int')
+
+    # mean_influence = np.mean(influences, axis=0).astype('int')  # derivative idea
+    mean_influence = np.mean(numerator, axis=0).astype('int')
+
+
+    if debug:
+        import os
+        if not os.path.exists('./out'):
+            os.makedirs('./out')
+
+        for i in range(n_pertubations):
+            print(pertubations[i], denominator[i].max())
+            io.imsave(f'out/{i}.png', Xpert[i].astype('uint8'))  # debugging
+            io.imsave(f'out/{i}_rec.png', Xpert_rec[i].astype('uint8'))  # debugging
+            io.imsave(f'out/{i}_numerator.png', numerator[i].astype('uint8'))  # debugging
+            io.imsave(f'out/{i}_denominator.png', denominator[i].astype('uint8'))  # debugging
+            io.imsave(f'out/{i}_influence.png', influences[i].astype('uint8'))  # debugging
+
+    return mean_influence
--- vaumc/reconstruction/reconstruction.py
+++ vaumc/reconstruction/reconstruction.py
@@ -3,41 +3,28 @@ from . import util
 
 
 def reconstruct_image(img, autoencoder):
-    print('-------------------------------')
     if img.ndim not in [2, 3]:
         raise Exception(f'Invalid number of image dimensions: {img.ndim}. ' \
                         'Expected: 2 or 3')
 
-    print('[1]')
     ysize, xsize = img.shape[0], img.shape[1]
-    print('[2]')
     n_channels = img.shape[2] if img.ndim == 3 else 1
-    print('[3]')
     norm_value = util.normalization_value(img)  # e.g., 255 for 8-bit images
 
-    print('[4]')
     X = img.reshape(1, ysize, xsize, n_channels)
-    print('[5]')
     X = util.normalize_image_set(X)
-    print('[6]')
     X = util.pad_by_autoencoder_input(X, autoencoder)
-    print('[7]')
 
     Xout = autoencoder.predict(X)  # shape (1, ysize, xsize, n_channels)
-    print('[8]')
     Xout = util.crop(Xout, shape=(ysize, xsize))
-    print('[9]')
 
     if n_channels == 1:
         img_out = Xout.reshape((ysize, xsize))
     else:
         img_out = Xout.reshape((ysize, xsize, n_channels))
-    print('[10]')
 
     img_out *= norm_value
-    print('[11]')
     img_out = img_out.astype(np.int32)
-    print('[12]')
 
     return img_out
 
@@ -50,7 +37,8 @@ def reconstruct_image_set(X, autoencoder):
                          'Expected: 3 (n_imgs, ysize, xsize) or 4 (n_imgs, ysize, xsize, n_channels)')
 
     if X.ndim == 3:
-        X = np.reshape(tuple(X.shape) + [1])  # (n_imgs, ysize, xsize, 1)
+        new_shape = tuple(list(X.shape) + [1])
+        X = np.reshape(X, new_shape)  # (n_imgs, ysize, xsize, 1)
 
     n_imgs, ysize, xsize, n_channels = X.shape
 
